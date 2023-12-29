780606e70e0dc48bfc27eb2f62518bdeb7195fe8
YaLian2003 <54329751+YaLian2003@users.noreply.github.com>
2023-12-29 00:12:49 -0800

    Create 7.1.java

---
+++ 7.1.java
@@ -0,0 +1,6 @@
+public class EightQueensSolver {
+    public static void main(String[] args) {
+        Queens queens = new Queens();
+        queens.placeQueens(0); // Start placing queens in the first column
+    }
+}
